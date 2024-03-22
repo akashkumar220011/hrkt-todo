d781fdbc867e5d2cf8bed38b52ac4ecda90aec97
akashkumar220011 <80808362+akashkumar220011@users.noreply.github.com>
2024-03-22 16:16:50 +0530

    Updated Code

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,34 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
+
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 app.post('/todo', function(req, res){
-
+     const createPayload = req.body;
+     const parsedPayload = createTodo.safeParse(createPayload);
+     if(!parsedPayload){
+        res.status(411).json({
+            msg: "You sent the wrong  inputs",
+        })
+        return;
+     }
+     //put it in mongodb
 })
 app.get('/todos', function(req, res){
 
 })
 app.put('/completed', function(req, res){
-
+    const  updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload){
+        res.status(411).json({
+            msg: "You sent the wrong  inputs",
+        })
+        return;
+    }
 })
 
 app.get('/', (req, res) => res.send('Hello World!'))
